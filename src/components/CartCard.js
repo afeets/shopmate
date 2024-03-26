b38fa612508c102df96ecdc38bd9de81983bae13
Andy Feetenby <AndyFeetenby@gmail.com>
2024-03-26 18:34:59 +0700

    Initial Commit - Boiler Plate

---
+++ src/components/CartCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const CartCard = () => {
+  return (
+    <div>
+      CART CARD
+    </div>
+  );
+}
---
+++ src/hooks/useTitle.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const UseTitle = () => {
+  return null;
+}
